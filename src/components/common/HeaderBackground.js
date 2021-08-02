210b807474c2ee7bf240704421101b4e1e8916f4
Alyfaraj <afarag840@gmail.com>
2021-08-03 00:27:15 +0200

    add/header-pattern

--- src/components/common/HeaderBackground.js
+++ src/components/common/HeaderBackground.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 import {Colors, Dimensions} from '../../theme';
 
 const HeaderBackground = ({children}) => {
   return (
-    <View style={styles.container}>
-      {children}
-      <View style={styles.borderBottom} />
+    <View style={{backgroundColor: Colors.primary}}>
+      <ImageBackground
+        style={{width: '100%', minHeight: Dimensions.DEVICE_HEIGHT * 0.2}}
+        imageStyle={{
+          opacity: 0.04,
+          resizeMode: 'cover',
+          height: '100%',
+          width: '100%',
+        }}
+        source={require('../../assets/images/backgroundpattern.png')}>
+        <View style={styles.container}>
+          {children}
+          <View style={styles.borderBottom} />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.primary,
+    // backgroundColor: Colors.primary,
     minHeight: Dimensions.DEVICE_HEIGHT * 0.2,
     width: Dimensions.DEVICE_WIDTH,
     //  justifyContent: 'space-around',
